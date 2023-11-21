b80319db2ac53df8b9e8f97b42e57382cc0deeb7
Ajdaai <assixa.chitanixa@gmail.com>
2023-11-21 12:58:58 +0100

    changes

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ if(isDarkMode){
 body.classList.add('dark-mode');
 }
 
-darkModeToggle.addEventListener('click',()=>{
-body.classList.toggle('dark-mode');
-const isDarkModeEnabled = body.classList.contains('dark-mode');
-localStorage.setItem('dark-mode',isDarkModeEnabled ? 'enabled' : 'disabled');
-});
+// darkModeToggle.addEventListener('click',()=>{
+// body.classList.toggle('dark-mode');
+// const isDarkModeEnabled = body.classList.contains('dark-mode');
+// localStorage.setItem('dark-mode',isDarkModeEnabled ? 'enabled' : 'disabled');
+// });
 /* just for git */
